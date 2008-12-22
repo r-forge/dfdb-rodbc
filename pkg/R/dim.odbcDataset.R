dc3145a936bacfb3ff4e7dcf12d4c8ace45f8cc8
ogcruz (Oswaldo Cruz) <oswaldo@fiocruz.br>
2008-12-22 07:15:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dfdb-rodbc@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dim.odbcDataset.R
+++ pkg/R/dim.odbcDataset.R
@@ -1,9 +1,12 @@
-# last modified 28 November 2008 by J. Fox 
+# last modified 22 Dec 2008 by OGC
 
 dim.odbcDataset <-
 function (x) {
-    # returns dimensions of odbcDataset object
-    if (!is.null(rownames(x))) c(length(row.names(x)), length(names(x)))
+
+    #returns dimensions of odbcDataset object
+
+    if (unclass(x)$rownames) c(length(row.names(x)), length(names(x)))
     else c(length(x[[1]]), length(names(x)))
+
     }
 
